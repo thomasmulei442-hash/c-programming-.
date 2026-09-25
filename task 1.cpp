a40ba917d33a442910010537de04d5d334e69946
thomasmulei442-hash <thomasmulei442@gmail.com>
2026-09-25 14:51:38 +0300

    Add files via upload

---
+++ task 1.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+//program to display bsc IT units Y1;sem1
+/*
+Name:Thomas Mulei
+Reg:CT100/G/30762/26
+Description:Bachelor of science in information technology
+Date:21/9/2026
+version 1
+*/
+#include <stdio.h>
+int main(){
+	printf("bachelor of information technology year 1 sem 1\n");
+	printf("1.fundamental of digital technology and artificial intelligence \n");
+	printf("2. analogue electronic \n");
+	printf("3. introduction to computer application \n");
+	printf("4. essentials of accounting \n");
+	printf("5.computer system and organization \n");
+	printf("6.communication and literacy skills\n");
+	printf("7.mathematics for science \n");
+	printf("8.introduction to programming and algorithm \n");
+	
+	return 0;
+ 
+}
