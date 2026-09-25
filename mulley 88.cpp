88166b971cf6262d9474dfc7fbf418a134637d1e
thomasmulei442-hash <thomasmulei442@gmail.com>
2026-09-25 16:41:06 +0300

    Add files via upload

---
+++ mulley 88.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	
+	float height;
+	double bankBalance;
+	int phoneNumber;
+	
+	printf("Enter your height:\n");
+	scanf("%f",&height);
+	
+	printf("Enter your bank Balance:\n");
+	scanf("%lf",&bankBalance);
+	
+	printf("Enter your phone number:\n");
+	scanf("%d",&phoneNumber);
+	
+	return 0;
+  }
